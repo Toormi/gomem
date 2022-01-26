9fef25628a6d52535179ae399935e3b4c535319e
Toormi <toormi@163.com>
2022-01-26 21:40:26 +0800

    fix: tool pprof

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
-	_ "go.uber.org/automaxprocs"
 	_map "gomem/map"
-	"net/http"
-	_ "net/http/pprof"
+	"os"
+	"runtime/pprof"
 )
 
 func main() {
+	f, err := os.Create("cpuprofile")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
+
 	//gobuntdb.TestBuntDb()
 	//gomem.TestMemDb()
 	//memory.TestMemory()
-	go http.ListenAndServe(":8080", nil)
+
 	_map.TestMap()
 }
